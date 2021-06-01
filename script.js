b2d1b8d8449ef7ccf058c7e6e2e288daf61c3474
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-06-01 18:47:12 -0300

    Arquivos adicionados via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+if(window.SimpleSlide) {
+new SimpleSlide ({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+});
+
+new SimpleSlide ({
+    slide: 'portfolio', 
+    time: 5000,
+    nav: true, 
+});
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
+
+if(window.SimpleForm){
+new SimpleForm ({
+    form:".formphp",
+    button:"#enviar",
+    erro:"<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente o e-mail contato@bikcraft.com.br</p></div>",
+    sucesso:"<div id='form-sucesso><h2>Formulário enviado com sucesso</h2><p>Em breve entraremos em contato.</p></div>",
+});
+}
